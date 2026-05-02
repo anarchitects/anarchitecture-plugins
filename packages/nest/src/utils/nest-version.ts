b073c5ffc81e15ba14e6b5bbb964e31f3b0e1b34
Johan Vrolix <johan@vrolix.eu>
2026-05-02 09:32:25 +0200

    test(nx-nest): Init generator dependency constraints and groups

--- packages/nest/src/utils/nest-version.ts
+++ packages/nest/src/utils/nest-version.ts
@@ -6,9 +6,18 @@
  * `@nestjs/schematics@next` or `@nestjs/cli@next`.
  */
 
+export const NEST_VERSION = 'next';
+
+export const NEST_COMMON_PACKAGE = '@nestjs/common';
+export const NEST_CORE_PACKAGE = '@nestjs/core';
+export const NEST_PLATFORM_EXPRESS_PACKAGE = '@nestjs/platform-express';
+export const NEST_TESTING_PACKAGE = '@nestjs/testing';
+
 export const NEST_SCHEMATICS_PACKAGE_NAME = '@nestjs/schematics';
-export const NEST_SCHEMATICS_PACKAGE = `${NEST_SCHEMATICS_PACKAGE_NAME}@next`;
+export const NEST_SCHEMATICS_VERSION = NEST_VERSION;
+export const NEST_SCHEMATICS_PACKAGE = `${NEST_SCHEMATICS_PACKAGE_NAME}@${NEST_SCHEMATICS_VERSION}`;
 
 export const NEST_CLI_PACKAGE_NAME = '@nestjs/cli';
-export const NEST_CLI_PACKAGE = `${NEST_CLI_PACKAGE_NAME}@next`;
+export const NEST_CLI_VERSION = NEST_VERSION;
+export const NEST_CLI_PACKAGE = `${NEST_CLI_PACKAGE_NAME}@${NEST_CLI_VERSION}`;
 export const NEST_CLI_BIN = 'nest';
